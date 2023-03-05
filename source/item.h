dc2460f513b04b7fa82aebc0b7e249fee55f6c09
Леонид Конашенков <konashenkov.lenya@gmail.com>
2023-03-05 23:26:40 +0300

    add spawn_item

--- source/item.h
+++ source/item.h
@@ -1,6 +1,7 @@
 #ifndef _ITEM_H_
 #define _ITEM_H_
 
+#include <stdlib.h>
 #include "coloredtext.h"
 
 enum ItemType {
@@ -17,5 +18,7 @@ struct Item {
     int value; // зависит от type. если health - то насколько восстановится
     int cost; // цена предмета
 };
+
+Item* spawn_item(ItemType type, int value, int id, ColoredText name, int cost);
 
 #endif
